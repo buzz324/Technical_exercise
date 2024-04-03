e431f0d579e9bbfb837804bb7ee839dcc89ea70f
Buzz Kho <buzz.kho@sap.com>
2024-04-03 22:59:10 +1300

    npm module deleted

--- dist/errorMessages.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorMessages = void 0;
-exports.errorMessages = {
-    noMatchingUsers: { message: 'No matching users found' },
-    invalidSurname: { message: 'Surname is too long or empty' },
-};
--- dist/usersData.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.users = void 0;
-exports.users = [
-    { surname: 'Smith', firstName: 'John', email: 'john.smith@example.com' },
-    { surname: 'Smith', firstName: 'Johns', email: 'john.smith@example.com' },
-    { surname: 'Johnson', firstName: 'Alice', email: 'alice.johnson@example.com' },
-];
